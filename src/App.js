7256993a6f5121a490fa5400d5d728f259ada978
bookhabit <qnrgoqlt@naver.com>
2022-09-15 15:40:59 +0900

    리덕스 미들웨어를 통한 비동기 작업관리 : redux-saga사용하여 비동기 카운터만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import SampleContainer from "./containers/SampleContainer";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 }
